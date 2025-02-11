1989011bd3462cd88f1709bd2be4d0ebc7f205b9
Elkin Arismendy <elk.orion@gmail.com>
2025-02-11 14:42:13 -0500

    Commit get_movies funcional

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import configKey
+from routes import Movie
 
 
 app = Flask(__name__)
@@ -11,6 +12,8 @@ def page_not_found(error):
 
 if __name__ == '__main__':
     app.config.from_object(configKey['development'])
+    # blueprints
+    app.register_blueprint(Movie.main, url_prefix='/api/movies')
     # Manejador de error
     app.register_error_handler(404, page_not_found)
     app.run()
